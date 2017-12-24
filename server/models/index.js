29039f816136b41308689e22719f62b1f3a15771
Iris Yu <iyucomms@gmail.com>
2017-12-23 22:58:28 -0800

    Update queries to reflect update to schema

--- server/models/index.js
+++ server/models/index.js
@@ -3,11 +3,13 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function (callback) {
-      let request = 'SELECT * FROM messages';
+      let request = 'SELECT messages.msgid, messages.text, messages.roomname, messages.userid FROM messages ORDER BY messages.msgid DESC';
       db.query(request, (err, results) => { callback(err, results); });
     }, // a function which produces all the messages
-    post: function (callback) {
-      let request = 'INSERT INTO messages(text, user, roomname) VALUE (?, (SELECT userid FROM users WHERE username = ? limit 1), ?)';
+    post: function (params, callback) {
+      let request = 'INSERT INTO messages(text, userid, roomname) VALUES (?, (SELECT userid FROM users WHERE username = ? limit 1), ?)';
+      // limit 1 is optional, but it just gives you the first one found
+      db.query(request, (err, results) => { callback(err, results); });
     } // a function which can be used to insert a message into the database
   },
 
@@ -18,8 +20,11 @@ module.exports = {
       db.query(request, (err, results) => { callback(err, results); });
     },
     post: function (params, callback) {
-      let request = 'INSERT INTO users(username) values (?)'; //userid auto-increments in the table
-      db.query(request, params,(err, results) => { callback(err, results); });
+      // calling it 'params' seems to be the convention
+      let request = 'INSERT INTO users(username) VALUES (?)';
+      //the '?' is an argument to the query function itself to provide parameters, hence the params argument
+      //userid auto-increments in the table
+      db.query(request, params, (err, results) => { callback(err, results); });
     }
   }
 };
