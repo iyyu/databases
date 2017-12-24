2b956a029e18850c05564a1fe24d2995373e8f08
Iris Yu <iyucomms@gmail.com>
2017-12-23 16:04:25 -0800

    Add schema for the two tables in db

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,19 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  msgid int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  roomname varchar(10),
+  text varchar(140) NOT NULL,
+  PRIMARY KEY (MessageUUID)
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users (
+  userid int NOT NULL AUTO_INCREMENT,
+  username varchar(10) NOT NULL,
+  PRIMARY KEY (UserID)
+);
 
 
 
