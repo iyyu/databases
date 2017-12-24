5842b932dda393a77e0a6c7e0808deb61f5e31f9
Iris Yu <iyucomms@gmail.com>
2017-12-23 23:00:45 -0800

    Update parameters for methods

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,20 +3,38 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
-      models.get
+      models.messages.get((err, data) => {
+        if (err) { console.log('error getting messages'); }
+        res.JSON(data);
+      });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
+      let params = [req.body.text, req.body.username, req.body.roomname];
+      // per mysql module, the params needs to be an array
 
+      models.messages.post(params, (err, data) => {
+        if (err) { console.log('error posting message'); }
+        res.sendStatus(201);
+        //201 for the creation of the row in table
+      });
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-
+      models.users.get((err, data) => {
+        if (err) { console.log('error getting users'); }
+        res.JSON(data);
+      });
     },
     post: function (req, res) {
-      
+      let params = [req.body.username];
+      models.users.post((err, data) => {
+        if (err) { console.log('error posting user'); }
+        res.sendStatus(201);
+        //201 for the creation of the row in table
+      });
     }
   }
 };
