921287d8a85ec141a9b3eeef2e84a42c1d0b117c
Iris Yu <iyucomms@gmail.com>
2017-12-23 22:57:16 -0800

    Update messages table to not include userid as foreign key

--- server/schema.sql
+++ server/schema.sql
@@ -8,16 +8,19 @@ CREATE TABLE messages (
   userid int NOT NULL,
   roomname varchar(10),
   text varchar(140) NOT NULL,
-  PRIMARY KEY (MessageUUID)
+  PRIMARY KEY (msgid)
+  
+  /* FOREIGN KEY (userid) REFERENCES users(userid)
+  for some reason, I couldn't add this as a foreign key
+  */
 );
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE users (
   userid int NOT NULL AUTO_INCREMENT,
   username varchar(10) NOT NULL,
-  PRIMARY KEY (UserID)
+  PRIMARY KEY (userid)
 );
-
 
 
 /*  Execute this file from the command line by typing:
