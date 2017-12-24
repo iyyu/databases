55d55e6c384e6f0f7a569220c6d4de6c31c8bdc5
Iris Yu <iyucomms@gmail.com>
2017-12-23 16:07:14 -0800

    Add client files

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,19 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+var connection = mysql.createConnection({
+  // host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
